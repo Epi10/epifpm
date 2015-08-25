81ce3c12d8db6bdab213ae8885e1aeca9836f5f3
Alvaro Leiva <aleivag@gmail.com>
2015-08-25 11:48:33 -0700

    no message

--- web/run.py
+++ web/run.py
@@ -1,5 +1,10 @@
 __author__ = 'aleivag'
 
+import os, sys
+
+sys.path.append(os.path.join(sys.path[0], '..'))
+
+from epifpm.zfm20 import Fingerprint
 
 from flask import Flask
 from flask.ext.mako import MakoTemplates, render_template
@@ -8,10 +13,11 @@ from flask.ext.mako import MakoTemplates, render_template
 app = Flask(__name__)
 mako = MakoTemplates(app)
 
+LOW_LEVEL_API = ['handshake']
 
 @app.route("/")
 def main():
-    return render_template('main_view.html')
+    return render_template('main_view.html', figerprint=Fingerprint, LOW_LEVEL_API=LOW_LEVEL_API)
 
 
 if __name__ == "__main__":
