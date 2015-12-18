169844d90b36cb05ec9b6dc0deb14203679f985d
Alvaro Leiva <aleivag@gmail.com>
2015-12-17 19:09:17 -0700

    no message

--- examples/copy.py
+++ examples/copy.py
@@ -12,20 +12,23 @@ f.connect()
 
 f.empty()
 
+
+print "get image 1"
 f.get_image_until()
 f.image_2_tz(buffer=1)
 image1 = f.up_image()['image']
 
-
+print "get image 2"
 f.get_image_until()
 f.image_2_tz(buffer=2)
 image2 = f.up_image()['image']
 
-
+print "Guardando modelo 1"
 f.store_model(id=1, buffer=1)
 
+print "buscando 1"
 f.get_image_until()
 f.image_2_tz(buffer=1)
-f.search(1)
+print f.search(1)
 
 f.empty()
